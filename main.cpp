433b957259a4a535656113dfe03569123ab4d42d
Fred Sheppard <stewart.sheppard@my.tccd.edu>
2020-02-25 01:49:01 +0000

    Declared Structs

--- main.cpp
+++ main.cpp
@@ -2,10 +2,36 @@
 #include <iostream>
 using namespace std;
 //declare structs above int main()
+struct timeOfDay
+{
+  short Hour;
+  short Minute;
+  short Second;
+};
 
+struct appDate
+{
+  short dayOfMonth;
+  short month;
+  int year;
+};
+
+struct event
+{
+  string eventName;
+  timeOfDay eventTime;
+  appDate eventDate;
+  bool isUrgent;
+  //save space here for our toString() override
+};
 
 
 int main() 
 {
-  
+  timeOfDay t;
+  t.Hour = 7;
+  t.Minute = 11;
+  t.Second = 17;
+
+  return 0;
 }

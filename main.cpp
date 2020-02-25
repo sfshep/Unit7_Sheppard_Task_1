694befddd190040985b6ce77eb20aab1c53cdc6d
Fred Sheppard <stewart.sheppard@my.tccd.edu>
2020-02-25 01:35:41 +0000

    Basic Setup

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
+//Unit7_Sheppard_Task_1 Dr_T 2-24-2020 STRUCTs C++
 #include <iostream>
+using namespace std;
+//declare structs above int main()
 
-int main() {
-  std::cout << "Hello World!\n";
+
+
+int main() 
+{
+  
 }
